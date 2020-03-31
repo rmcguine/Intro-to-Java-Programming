794ade84e59cb1d95b739e591cfce48a535fe7fd
Rob McGuine <rmcguine@gmail.com>
2020-03-31 14:26:28 -0400

    Update Exercise_03_03.java

--- Exercises03/Exercise_03_03.java
+++ Exercises03/Exercise_03_03.java
@@ -1,5 +1,3 @@
-package Exercises03;
-
 /* (Algebra: solve 2 * 2 linear equations) A linear equation can be solved using Cramer’s rule 
 given in Programming Exercise 1.13. Write a program that prompts the user to enter a, b, c, d, e, 
 and f and displays the result. If ad - bc is 0, report that “The equation has no solution.”
