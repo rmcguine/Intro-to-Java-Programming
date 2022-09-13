e076a6e2e38cf36da06618c3ce98ca6c1fcb3ba5
Rob McGuine <rmcguine@gmail.com>
2022-09-13 14:16:55 -0400

    Update Test.java

--- Exercises03/Exercise_03_01/Test.java
+++ Exercises03/Exercise_03_01/Test.java
@@ -1,1 +1,9 @@
-// TestTest
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Test....Test....");
+    }
+}
